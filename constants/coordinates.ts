f929bd778e5da40948973b8422cd556aec36b2c1
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-02-26 11:18:57 -0500

    feat: add coordinates

---
+++ constants/coordinates.ts
@@ -0,0 +1,169 @@
+export const routeCoordinates = [
+  {
+    latitude: -12.053787,
+    longitude: -77.085094,
+  },
+  {
+    latitude: -12.053639,
+    longitude: -77.086053,
+  },
+  {
+    latitude: -12.053498,
+    longitude: -77.086234,
+  },
+  {
+    latitude: -12.053473,
+    longitude: -77.086424,
+  },
+  {
+    latitude: -12.054661,
+    longitude: -77.086551,
+  },
+  {
+    latitude: -12.054780,
+    longitude: -77.086634,
+  },
+  {
+    latitude: -12.054917,
+    longitude: -77.085430,
+  },
+  {
+    latitude: -12.054953,
+    longitude: -77.085349,
+  },
+  {
+    latitude: -12.055074,
+    longitude: -77.085327,
+  },
+  {
+    latitude: -12.055071,
+    longitude: -77.085212,
+  },
+  {
+    latitude: -12.055341,
+    longitude: -77.084751,
+  },
+  {
+    latitude: -12.055721,
+    longitude: -77.084786,
+  },
+  {
+    latitude: -12.055721,
+    longitude: -77.084786,
+  },
+  {
+    latitude: -12.057829,
+    longitude: -77.085746,
+  },
+  {
+    latitude: -12.058055,
+    longitude: -77.085708,
+  },
+  {
+    latitude: -12.058615,
+    longitude: -77.084900,
+  },
+  {
+    latitude: -12.058734,
+    longitude: -77.084839,
+  },
+  {
+    latitude: -12.058904,
+    longitude: -77.084555,
+  },
+  {
+    latitude: -12.059075,
+    longitude: -77.084619,
+  },
+  {
+    latitude: -12.060889,
+    longitude: -77.084209,
+  },
+  {
+    latitude: -12.060031,
+    longitude: -77.079776,
+  },
+  {
+    latitude: -12.059809,
+    longitude: -77.079624,
+  },
+  {
+    latitude: -12.056674,
+    longitude: -77.080394,
+  },
+  {
+    latitude: -12.056428,
+    longitude: -77.080587,
+  },
+  {
+    latitude: -12.056415,
+    longitude: -77.080812,
+  },
+  {
+    latitude: -12.056129,
+    longitude: -77.081096,
+  },
+  {
+    latitude: -12.055291,
+    longitude: -77.082504,
+  },
+  {
+    latitude: -12.055494,
+    longitude: -77.082819,
+  },
+  {
+    latitude: -12.055463,
+    longitude: -77.083237,
+  },
+  {
+    latitude: -12.054775,
+    longitude: -77.083602,
+  },
+  {
+    latitude: -12.054600,
+    longitude: -77.083848,
+  },
+  {
+    latitude: -12.053787,
+    longitude: -77.085094,
+  },
+]
+
+export const paradas = [
+  {
+    latitude: -12.054103,
+    longitude: -77.084625,
+  },
+  {
+    latitude: -12.053666,
+    longitude: -77.085840,
+  },
+  {
+    latitude: -12.054874,
+    longitude: -77.086070,
+  },
+  {
+    latitude: -12.056296,
+    longitude: -77.085104,
+  },
+  {
+    latitude: -12.060130,
+    longitude: -77.084420,
+  },
+  {
+    latitude: -12.060822,
+    longitude: -77.082954,
+  },
+  {
+    latitude: -12.059580,
+    longitude: -77.079624,
+  },
+  {
+    latitude: -12.057440,
+    longitude: -77.080171,
+  },
+  {
+    latitude: -12.055545,
+    longitude: -77.082058,
+  },
+]
